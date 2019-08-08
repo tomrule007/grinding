77e2b84428628eaf87fd7a4ddcc3fec4cd328161
Thomas Herzog <Tomrule007@gmail.com>
2019-08-07 18:35:28 -0700

    added material therme provider to color buton (red/green)

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,8 +1,19 @@
 // @flow
 import React, { Component } from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import lightGreen from '@material-ui/core/colors/green';
+import { ThemeProvider } from '@material-ui/styles';
 import TheButton from '../components/TheButton';
 
 type Props = {};
+
+const theme = createMuiTheme({
+  palette: {
+    primary: red, // offState
+    secondary: lightGreen // onState
+  }
+});
 
 const electron = require('electron');
 
@@ -12,6 +23,10 @@ export default class HomePage extends Component<Props> {
   props: Props;
 
   render() {
-    return <TheButton />;
+    return (
+      <ThemeProvider theme={theme}>
+        <TheButton />
+      </ThemeProvider>
+    );
   }
 }
