f4db2a35f93629fd08c1ac9534fc8cb947b5a20f
Thomas Herzog <Tomrule007@gmail.com>
2019-08-20 11:55:30 -0700

    updated routes to match file name change.
    also removed unused counterPage

--- app/Routes.js
+++ app/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import routes from './constants/routes';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
+import ButtonPage from './containers/ButtonPage';
 import LogPage from './containers/LogPage';
 import ChartPage from './containers/ChartPage';
 import StartPage from './containers/StartPage';
@@ -11,7 +11,7 @@ import StopPage from './containers/StopPage';
 export default () => (
   <App>
     <Switch>
-      <Route exact path={routes.HOME} component={HomePage} />
+      <Route exact path={routes.BUTTON} component={ButtonPage} />
       <Route path={routes.LOG} component={LogPage} />
       <Route path={routes.CHART} component={ChartPage} />
       <Route path={routes.START} component={StartPage} />
