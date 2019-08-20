1ac37ec81cce79c8851d78cf3b6e940849ce9139
Thomas Herzog <Tomrule007@gmail.com>
2019-08-20 11:43:22 -0700

    merged TheButton component with HomePage

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,12 +1,16 @@
-// @flow
 import React, { Component } from 'react';
+import Fab from '@material-ui/core/Fab';
+
+// style imports
 import { createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import lightGreen from '@material-ui/core/colors/green';
 import { ThemeProvider } from '@material-ui/styles';
-import TheButton from '../components/TheButton';
 
-type Props = {};
+// utils
+import localStore from '../utils/localStore';
+import { startWin, stopWin } from '../WindowFactory';
+import { ON_COLOR, OFF_COLOR } from '../constants/defaultConfig';
 
 const theme = createMuiTheme({
   palette: {
@@ -14,15 +18,75 @@ const theme = createMuiTheme({
     secondary: lightGreen // onState
   }
 });
+class TheButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      start: null,
+      grinding: false,
+      color: OFF_COLOR,
+      intervalID: null,
+      timeDisplay: 'start'
+    };
+  }
 
-export default class HomePage extends Component<Props> {
-  props: Props;
+  startTimer() {
+    const intervalID = setInterval(() => this.timerTick(Date.now()), 1000);
+
+    this.setState({ intervalID, timeDisplay: '0' });
+  }
+
+  timerTick(currentTime) {
+    const { start } = this.state;
+    const seconds = Math.floor((currentTime - start) / 1000);
+    const minutes = Math.floor(seconds / 60);
+
+    this.setState({ timeDisplay: seconds < 60 ? seconds : minutes });
+  }
+
+  stopTimer() {
+    const { intervalID } = this.state;
+    clearInterval(intervalID);
+    this.setState({ timeDisplay: 'start' });
+  }
+
+  toggleGrind(clickTime) {
+    let { grinding, start, color } = this.state;
+
+    // toggle state
+    grinding = !grinding;
+
+    if (grinding) {
+      // starting grinding session
+      color = ON_COLOR;
+      start = clickTime;
+      localStore.set('session', { start: clickTime });
+      this.startTimer();
+      startWin.create();
+    } else {
+      // stopping grinding session
+      color = OFF_COLOR;
+      localStore.update('session', {
+        stop: clickTime,
+        gTime: clickTime - start
+      });
+      this.stopTimer();
+      stopWin.create();
+    }
+
+    this.setState({ grinding, start, color });
+  }
 
   render() {
+    const { color, timeDisplay } = this.state;
     return (
       <ThemeProvider theme={theme}>
-        <TheButton />
+        <Fab color={color} onClick={() => this.toggleGrind(Date.now())}>
+          {timeDisplay}
+        </Fab>
       </ThemeProvider>
     );
   }
 }
+
+export default TheButton;
