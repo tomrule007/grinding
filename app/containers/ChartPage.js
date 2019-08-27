31bc6ff98057bf9674be6175c1a7c7d6c2607048
Thomas Herzog <Tomrule007@gmail.com>
2019-08-26 20:24:48 -0700

    refactored chartPage from class component to functional component

--- app/containers/ChartPage.js
+++ app/containers/ChartPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   BarChart,
   CartesianGrid,
@@ -14,48 +14,39 @@ import localStore from '../utils/localStore';
 const formatStartDate = ms => new Date(ms).toLocaleDateString('en-US');
 const formatTime = ms => Math.floor(ms / 1000); // Ms to minutes
 
-export default class ChartPage extends Component {
-  constructor(props) {
-    super(props);
+function ChartPage() {
+  const rawData = localStore.get('log');
+  const data = Object.entries(
+    rawData.reduce((acc, { gTime, start }) => {
+      const formatedStart = formatStartDate(start);
+      if (acc[formatedStart]) {
+        acc[formatedStart] += gTime;
+      } else {
+        acc[formatedStart] = gTime;
+      }
+      return acc;
+    }, {})
+  ).map(keyVal => ({ date: keyVal[0], time: formatTime(keyVal[1]) }));
 
-    // @TODO log wrapper that tells children to update when log data changes
-    // and also allow table to update log data store.
-    const rawData = localStore.get('log');
-    const totalDailyGrindTime = Object.entries(
-      rawData.reduce((acc, { gTime, start }) => {
-        const formatedStart = formatStartDate(start);
-        if (acc[formatedStart]) {
-          acc[formatedStart] += gTime;
-        } else {
-          acc[formatedStart] = gTime;
-        }
-        return acc;
-      }, {})
-    ).map(keyVal => ({ date: keyVal[0], time: formatTime(keyVal[1]) }));
+  console.log(rawData);
+  return (
+    <div>
+      <DateRangeToolbar />
 
-    this.state = { data: totalDailyGrindTime, rawData };
-  }
-
-  render() {
-    const { data, rawData } = this.state;
-    console.log(rawData);
-    return (
-      <div>
-        <DateRangeToolbar data={rawData} onChange={this.setDateRange} />
-
-        <div style={{ textAlign: 'center' }}>
-          <h1>Your Time Grinding</h1>
-        </div>
-
-        <BarChart width={730} height={250} data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="time" fill="#8884d8" />
-        </BarChart>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Your Time Grinding</h1>
       </div>
-    );
-  }
+
+      <BarChart width={730} height={250} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="time" fill="#8884d8" />
+      </BarChart>
+    </div>
+  );
 }
+
+export default ChartPage;
